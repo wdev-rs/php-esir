96b0b1c1305c63a9aa37e034d07cd84d8c6211d0
Augusztinovics Janos <lancing0@gmail.com>
2022-02-03 16:11:43 +0000

    Request classok valtozoi

--- src/Request/Pay.php
+++ src/Request/Pay.php
@@ -4,4 +4,59 @@ namespace WdevRs\PhpEsir\Request;
 //Az egy fizetesfajta meghatarozasara es a fizetes osszege
 class Pay {
 
+    //az amountnak 0 vagy nagyobbnak kell lenie
+    protected $amount;
+
+    /*a payment type lehet integer vagy string mindkettot elfogadja a proceszor
+    paymentType ertekei lehetnek a kovetkezok int - string formaban
+    0 - Other
+    1 - Cash
+    2 - Card
+    3 - Check
+    4 - WireTransfer
+    5 - Voucher
+    6 - MobileMoney */   
+    protected $paymentType;
+
+    public function __construct($amount, $payType){
+        $this->amount = $amount;
+        $this->paymentType = $payType;
+    }
+
+    public function IsValid(){
+
+        //az osszegnek szamnak kell lenie, ha nem az bisztos nem jo
+        if (is_numeric($this->amount)){
+
+            //Nem lehet kevesebb mint nulla, megha refund akkor is pozitivba kell megadni
+            if ($this->amount < 0){
+                return false;
+            }
+
+            //Ez a maximum osszeg amit meg lehet adni
+            if ($this->amount > 999999999999.99){
+                return false;
+            }
+        }else{
+
+            //Nem szam volt az amount
+            return false;
+        }
+
+        //A paytype lehet szam vagy string itt megnezuk szam e
+        if (is_numeric($this->paymentType)){
+
+            //Ha szam 0 es 6 kozott kell lenie
+            if ($this->paymentTape < 0 || $this->paymentType > 6){
+                return false;
+            }
+
+            //Nem szam volt, tehat osszehasonlitsuk a stringekkel, a kis es nagy beture vigyazni kell, mert erzekeny ra a proceszor
+        }elseif ($this->paymentType !== 'Other' && $this->paymentType !== 'Cash' && $this->paymentType !== 'Card' && $this->paymentType !== 'Check' && $this->paymentType !== 'WireTransfer' && $this->paymentType !== 'Voucher' && $this->paymentType !== 'MobilMoney'){
+            return false;
+        }
+
+        //Ha egyik ellenorzes sem kuldott vissza false ot talan minden jo
+        return true;
+    }
 }
--- src/Request/Request.php
+++ src/Request/Request.php
@@ -3,5 +3,49 @@ namespace WdevRs\PhpEsir\Request;
 
 //A vegleges szamlakerelmi modell amit kell majd elkuldeni
 class Request {
+    //A dateAndTimeOfIssue -t csak is es kizarolag AVANS PRODAJA esetebe kell megadni, minden mas esetbe null kell, hogy legyen
+    protected $dateAndTimeOfIssue;
+
+    //lenyegtelen adat lehet null vagy valakinek a neve aki elmeletileg a kaszan dolgozik alltalaba null
+    public $cashier = null;
+
+    //lehet megadni, de a formatuma macerans meghatarozott kodszamok vannak a kulonbozo azonositashoz, nemely szamlan viszont kotelezo :(
+    public $buyerId = null;
+
+    //egyaltalan nem kotelezo, de ha meg van adva kotelezo, hogy legyen buyerId is megadva ugyszinten kodszamokkal megy
+    public $buyerCostCenterId = null;
+
+    //FONTOS lehet szam vagy string kovetkezok lehetnek int - string formaban:
+        //0 - Normal  
+        //1 - ProForma   
+        //2 - Copy       
+       // 3 - Training   
+       // 4 - Advance
+    
+    public $invoiceType;
+
+    //FONTOS lehet szam vagy string kovetkezok lehetnek int - string formaban
+    //0 - Sale
+    //1 - Refund
+    public $transactionType;
+
+    //Array a Pay classokbol, akkor is array kell, hogy legyen, ha csak egy eleme van
+    protected $payment;
+
+    //Ez nem az aminek gondolja az ember, ez a PROGRAM regisztracios kodja, ugyhogy hardcodolva lesz a vegen :)
+    protected $invoiceNumber;
+
+    //Ha egy szamlat egy masik szamlara hivatkozva adnak ki a kovetkezo ket mezo kotelezo
+    //Copy es refundnal mindenfelekeppen, vagy normal, ami tartalmaz Avans fizetest, vagy eloszamlara hivatkozik
+    //A referentDocumentNumber a szamla szama, ami a responsba visszajott
+    //A DT pedig az ideje, vigyazni kell a datum formatumara, stringbe kell keldeni yyyy/MM/DD hh:mm:ss.ms formatumban
+    public $referentDocumentNumber;
+    public $referentDocumentDT;
+
+    //Ez hogy kersz e vissza QR kodott, meg journalt ezzel a bealitassal journal jon QR kod nem
+    public $options = array("omitQRCodeGen" => '1', "omitTextualRepresentation" => '0');
+
+    //array az Item class bol akkor is array, ha csak egy item van
+    protected $items;
 
 }
