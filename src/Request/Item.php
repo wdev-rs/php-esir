7e2612a3fa1a1ef7dc59a53471deee961e5145cc
Augusztinovics Janos <lancing0@gmail.com>
2022-02-06 08:59:28 +0000

    Atdolgozva az item class + tests

--- src/Request/Item.php
+++ src/Request/Item.php
@@ -1,28 +1,41 @@
 <?php
 namespace WdevRs\PhpEsir\Request;
 
-//Egy item adatai amit a szamlan meg kell adni
-//A szamlara items[] megy, megha csak egy item van is benne
+/**
+ * Define one item in Invoice.
+ * 
+ * Can define an item more than one way, the easiest way throu its constructor send all data as associative array
+ * That way the class will be done and ready to use right after its creation
+ * The ather way, to send its constructor just its name (and optionally gtin), and the rest set throu functions later
+ * Before use the item, the class have isValid function to check all data in item have and valid.
+ * 
+ * @var string|null $gtin  The barcode of item must be 8-14 character or null.
+ * @var string $name The name of the item, after the name must hav /unit (there is a fuction to add that later).
+ * @var float|double|int $quantity The quantity of item.
+ * @var float|double $unitPrice The price for one unit.
+ * @var array $labels Tax category labels for item.
+ * @var float|double $totalPrice The total price of item (quantity * unitPrice).
+ */
 class Item {
 
     //barcodja az itemnek 8-14 character, mindeg string
     //Ha nincs gtin null ertek kell
-   public $gtin = null;
+   protected $gtin = null;
 
    //A neve kovetve/unit pl kabat/kom
-   public $name;
+   protected $name;
 
    //Hany darab
-   public $quantity;
+   protected $quantity;
 
    //Egy darab mennyibe kerul
-   public $unitPrice;
+   protected $unitPrice;
 
    //Mindeg egy array, megha csak egy label van pl ['A']
-   public $labels;
+   protected $labels;
 
    //quantity*unit price 
-   public $totalAmount;
+   protected $totalAmount;
 
    //ha lekuljek az item adatait(vagy reszadatait) az obj keszitesenel
    public function __construct(  $itemData = null) {
@@ -33,26 +46,141 @@ class Item {
        $this->unitPrice = $itemData['unitPrice'] ?? null;
        $this->labels = $itemData['labels'] ?? [];
        if ($this->quantity !== null && $this->unitPrice !== null){
-           $this->totalAmount = $this->quantity * $this->unitPrice;
+           if (is_numeric($this->quantity) && is_numeric($this->unitPrice)){
+
+           //Ha a quantity es unit price szam akkor szorozuk ossze a total amountot
+             $this->totalAmount = $this->quantity * $this->unitPrice;
+             $this->totalAmount = round($this->totalAmount, 2, PHP_ROUND_HALF_UP);
+           }else{
+            $this->totalAmount = null;
+           }
        }else{
            $this->totalAmount = null;
        }
    }
 
+   //Utolag is lehessen beallitani az egyseg arat
+   public function setUnitPrice($price){
 
-   //Ha utolag adjak meg a quantityt es a unitpricet ezzel be lehet allitani a totalAmpuntot
-   public function countTotal(){
-    if ($this->quantity !== null && $this->unitPrice !== null){
-        $this->totalAmount = $this->unitPrice * $this->quantity;
-        return true;
-    }else{
-        return false;
-    }      
+       //Ha nem szamot kuldott, nem foglalkozunk vele
+       if (is_numeric($price)){
+
+           //Az ar nem lehet negativ vagy nulla
+           if ($price > 0){
+            $this->unitPrice = $price;
+            return true;
+           }else{
+               return false;
+           }
+       }else{
+           return false;
+       }
    }
+
+    //Ha utolag adjak meg a quantityt es a unitpricet ezzel be lehet allitani a totalAmpuntot
+    public function countTotal(){
+        if ($this->quantity !== null && $this->unitPrice !== null){
+            if (is_numeric($this->quantity) && is_numeric($this->unitPrice)){
+              $this->totalAmount = $this->unitPrice * $this->quantity;
+              $this->totalAmount = round($this->totalAmount, 2, PHP_ROUND_HALF_UP);
+            return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }      
+    }
+
+   //Ezzel lehet megadni, vagy a megadott quantityhez hozzaadni quantity t
+   public function addQuantity($quant){
+
+       //Ha meg nem volt megadva quantity
+       if ( $this->quantity === null){
+
+           //A lekuldott quantity szam e
+           if (is_numeric($quant)){
+
+               //Mivel elso quantity, tehat meg nem volt hozzaadva nagyobbnak kell, hogy legyen nullanal
+               if ($quant > 0){
+                  $this->quantity = $quant;
+
+                  //Miutan kaptunk quantityt kiszamoljuk a totalt, ha a funkcio false ot kuld vissza, nincs unit price megadva
+                  if ($this->countTotal()){
+                    return true;
+                  }else{
+                    return false;
+                  }
+               }else{
+                   //A megadott quant nulla, vagy negativ, nem lehet az elso quant nulla vagy negativ
+                   return false;
+               }
+           }else{
+               //Nem szamot kuldott quantnak, a felhasznalo valamit eltolt 
+               return false;
+           }
+
+        //Mar volt quantunk megadva, az uj quantot hozzaadjuk a regihez es ujraszamoljuk a totalt
+       }else{
+           if (is_numeric($quant)){
+
+              //Hozzaadjuk a kapott quantot a regi quanthoz(kivonjuk, ha negativ a szam)
+               $this->quantity += $quant;
+
+               //Leellenorizuk, hogy a quant nagyobb e nullanal
+               if ($this->quantity > 0){
+
+                   //Ujraszamoljuk a totalt, ha nem sikerul nincs unit price megadva
+                   if ($this->countTotal()){
+                       return true;
+                   }else{
+                       return false;
+                   }
+               }else{
+                   
+                //A quantity nulla vagy negativ volt, igy null erteket adunk a quantitynek meg a totalnak, mintha meg sem lett volna adva
+                $this->quantity = null;
+                $this->totalAmount = null;
+
+                //Attol fuggetlenul, hogy lenullaztuk az erteket a muvelet sikeres volt (Ha az volt a cel hogy toroljuk a quantot es totalt)
+                return true;
+               }
+           }else{
+               //Nem szamot kuldott quantnak
+               return false;
+           }
+
+       }
+   }
+
+
+  
 
    //Ezzel lehet mertekegyseget a nevhez hozzaadni, ha nem adtuk alapbol oda
    public function addUnitToItemname($unit){
-       $this->name .= '/'. $unit;
+       if ($this->name !== null){
+        $this->name .= '/'. $unit;
+        return true;
+       }else{
+           return false;
+       }
+       
+   }
+
+   //A labels array hoz lehet egyesivel adni labeleket
+   public function addOneLabel($label){
+
+       //A label egy character, de string formaban, ha hosszabb, vagy nem string nem adjuk az arrayhoz
+       if (is_string($label)){
+           if (strlen($label) === 1){
+            $this->labels[] = $label;
+            return true;
+           }else{
+               return false;
+           }
+       }else{
+           return false;
+       }
    }
 
 
@@ -73,15 +201,34 @@ class Item {
         if (strlen($this->name) > 2048){
             return false;
         }
+        //Van e mertekegyseg csatolva a nevhez
+        $sec = explode('/', $this->name);
+        if (count($sec) < 2){
+            return false;
+        }
     }
 
     //quantity kotelezo min 0.001 max 9999999999.999 lehet
     if ($this->quantity == null){
         return false;
     }
+    if (is_numeric($this->quantity) !== true){
+        return false;
+    }
+    if ($this->quantity < 0.001 || $this->quantity > 9999999999.999){
+        return false;
+    }
 
     //unitprice kotelezo
     if ($this->unitPrice == null){
+        return false;
+    }
+    //Szamnak kell, hogy legyen
+    if (is_numeric($this->unitPrice) !== true){
+        return false;
+    }
+    //Nem lehet nulla, vagy nagyobb 999999999999.99
+    if ($this->unitPrice <= 0 || $this->unitPrice > 999999999999.99){
         return false;
     }
 
--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -30,7 +30,7 @@ class ItemTest extends TestCase
             'Valid item data' => [
                 'itemData' => [
                 'gtin' => null,
-                'name' => 'Test name',
+                'name' => 'Test name/kom',
                 'quantity' => 1,
                 'unitPrice' => 10,
                 'labels' => ['A']
@@ -46,8 +46,193 @@ class ItemTest extends TestCase
                 'labels' => ['A']
                 ],
                 'expectedResult' => false
+            ],
+            'Name whitout unit' => [
+                'itemData' => [
+                'gtin' => null,
+                'name' => 'Test item',
+                'quantity' => 1,
+                'unitPrice' => 10,
+                'labels' => ['A']
+                ],
+                'expectedResult' => false
+            ],
+            'Valid gtin' => [
+                'itemData' => [
+                'gtin' => '12345678',
+                'name' => 'Test item/kg',
+                'quantity' => 1,
+                'unitPrice' => 10,
+                'labels' => ['A']
+                ],
+                'expectedResult' => true
+            ],
+            'Invalid gtin' => [
+                'itemData' => [
+                'gtin' => '1234567',
+                'name' => 'Test item/kg',
+                'quantity' => 1,
+                'unitPrice' => 10,
+                'labels' => ['A']
+                ],
+                'expectedResult' => false
+            ],
+            'Invalid unitPrice' => [
+                'itemData' => [
+                'gtin' => '12345678',
+                'name' => 'Test item/kg',
+                'quantity' => 1,
+                'unitPrice' => 0,
+                'labels' => ['A']
+                ],
+                'expectedResult' => false
+            ],
+            'No labels' => [
+                'itemData' => [
+                'gtin' => '12345678',
+                'name' => 'Test item/kg',
+                'quantity' => 1,
+                'unitPrice' => 10,
+                'labels' => []
+                ],
+                'expectedResult' => false
+            ],
+            'Invalid quantity' => [
+                'itemData' => [
+                'gtin' => '12345678',
+                'name' => 'Test item/kg',
+                'quantity' => -1,
+                'unitPrice' => 10,
+                'labels' => ['A']
+                ],
+                'expectedResult' => false
             ]
         ];
+    }
+
+    /**
+     * @dataProvider providePriceData
+     */
+    public function testsetUnitPrice($priceData, $expectedResult)
+    {
+        $item = new Item();
+        $this->assertEquals($expectedResult, $item->setUnitPrice($priceData));
+    }
+
+    public function providePriceData(){
+        return [
+            'Valid unitPrice' => [
+                'priceData' =>  10.5,
+                'expectedResult' => true
+
+            ],
+            'Null unitPrice' => [
+                'priceData' =>  null,
+                'expectedResult' => false
+
+            ],
+            'Negative unitPrice' => [
+                'priceData' =>  -10.5,
+                'expectedResult' => false
+
+               ]
+            ];
+    }
+
+     /**
+     * @dataProvider provideQuantData
+     */
+    public function testaddNewQuantity($quantData, $expectedResult)
+    {
+        $item = new Item();
+        $item -> setUnitPrice(10);
+        $this->assertEquals($expectedResult, $item->addQuantity($quantData));
+    }
+
+     /**
+     * @dataProvider provideAddQuantData
+     */
+    public function testaddMoreQuantity($quantData, $expectedResult)
+    {
+        $item = new Item();
+        $item -> setUnitPrice(10);
+        $item->addQuantity(1);
+        $this->assertEquals($expectedResult, $item->addQuantity($quantData));
+    }
+
+    public function provideQuantData(){
+        return [
+            'Valid quant' => [
+                'quantData' =>  2,
+                'expectedResult' => true
+
+            ],
+            'Null quant' => [
+                'quantData' =>  null,
+                'expectedResult' => false
+
+            ],
+            'Negative quant' => [
+                'quantData' =>  -2,
+                'expectedResult' => false
+
+               ]
+            ];
+    }
+
+    public function provideAddQuantData(){
+        return [
+            'Valid quant' => [
+                'quantData' =>  2,
+                'expectedResult' => true
+
+            ],
+            'Null quant' => [
+                'quantData' =>  null,
+                'expectedResult' => false
+
+            ],
+            'Negative quant' => [
+                'quantData' =>  -2,
+                'expectedResult' => true
+
+               ]
+            ];
+    }
+
+    /**
+     * @dataProvider provideLabelData
+     */
+    public function testaddLabel($labelData, $expectedResult)
+    {
+        $item = new Item();
+        
+        $this->assertEquals($expectedResult, $item->addOneLabel($labelData));
+    }
+
+    public function provideLabelData(){
+        return [
+            'Valid label' => [
+                'labelData' =>  'A',
+                'expectedResult' => true
+
+            ],
+            'Null label' => [
+                'labelData' =>  null,
+                'expectedResult' => false
+
+            ],
+            'Nonstring label' => [
+                'labelData' =>  2,
+                'expectedResult' => false
+
+            ],
+            'Long label' => [
+                'labelData' =>  'Huhu',
+                'expectedResult' => false
+
+               ]
+            ];
     }
 
 }
