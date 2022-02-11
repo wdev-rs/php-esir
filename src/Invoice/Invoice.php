16fb1e85a4289738e2520b233a19a956ecf4478b
Augusztinovics Janos <lancing0@gmail.com>
2022-02-11 05:39:35 +0000

    Az invoice geterekkel

--- src/Invoice/Invoice.php
+++ src/Invoice/Invoice.php
@@ -43,4 +43,111 @@ class Invoice {
         }
     }
 
+    public function getRequestedBy(){
+        return $this->requestedBy;
+    }
+
+    public function getSdcDateTime(){
+        return $this->sdcDateTime;
+    }
+
+    public function getInvoiceCounter(){
+        return $this->invoiceCounter;
+    }
+
+    public function getInvoiceCounterExtension(){
+        return $this->invoiceCounterExtension;
+    }
+
+    public function getInvoiceNumber(){
+        return $this->invoiceNumber;
+    }
+
+    //Egy array a Tax Itemekbol
+    public function getTaxItems(){
+        return $this->taxItems;
+    }
+
+    //Ezt kell mindenfelekeppen a szamlahoz csatolni!!!!
+    public function getVerificationUrl(){
+        return $this->verificationUrl;
+    }
+
+    //Ugyis null
+    public function getVerificationQRCode(){
+        return $this->verificationQRCode;
+    }
+
+    //A fiszkalis resz a szamlanak
+    public function getJournal(){
+        return $this->journal;
+    }
+
+    //Ertelmetlen, mindeg success, ha nem ugyis error response volt
+    public function getMessages(){
+        return $this->messages;
+    }
+
+    public function getSignedBy(){
+        return $this->signedBy;
+    }
+
+    //Ugyis olvashatatlan, de ha valaki akar oszeviszasagot nezni
+    public function getEncryptedInternalData(){
+        return $this->encryptedInternalData;
+    }
+
+    //Mint a fentebbi
+    public function getSignature(){
+        return $this->signature;
+    }
+
+    //Osszesen hany szamla lett kiadva
+    public function getTotalCounter(){
+        return $this->totalCounter;
+    }
+
+    //Az adott szamlatipusbol, amihez ez a szamla tartozik, hany szamla lett edig kiadva
+    public function getTransactionTypeCounter(){
+        return $this->transactionTypeCounter;
+    }
+
+    public function getTotalAmount(){
+        return $this->totalAmount;
+    }
+
+    //Az adocsoport kodja, ami alapjan let az ado szamolva a szamlan
+    public function getTaxGroupRevision(){
+        return $this->taxGroupRevision;
+    }
+
+
+    //Ha elfelejtenenk a cegunk nevet itt leelenorizhetjuk
+    public function getBusinessName(){
+        return $this->businessName;
+    }
+
+    //Valojaba a ceg PIB je
+    public function getTin(){
+        return $this->tin;
+    }
+
+    public function getLocationName(){
+        return $this->locationName;
+    }
+
+    public function getAddress(){
+        return $this->address;
+    }
+
+    //A varos neve
+    public function getDistrict(){
+        return $this->district;
+    }
+
+    //A regisztracios kodja a programnak amelyik alairta a szamlat
+    public function getMrc(){
+        return $this->mrc;
+    }
+
 }
